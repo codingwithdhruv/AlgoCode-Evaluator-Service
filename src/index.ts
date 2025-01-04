442a977936070f67c7ece0f81840da7eb6e04e6e
codingwithdhruv <techdhruvspace@gmail.com>
2025-01-05 02:40:11 +0530

    Added BullBoard

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express, { Express } from "express";
 
+import bullBoardAdapter from "./config/bullBoardConfig";
 import serverConfig from "./config/serverConfig";
 import sampleQueueProducer from "./producers/sampleQueueProducer";
 import apiRouter from "./routes";
@@ -9,15 +10,17 @@ import SampleWorker from "./workers/SampleWorker";
 const app: Express = express();
 
 app.use('/api', apiRouter);
+app.use('/ui', bullBoardAdapter.getRouter());
 
 app.listen(serverConfig.PORT, () => {
     console.log(`Server Started at: ${serverConfig.PORT}`);
 });
+console.log(`Bullboard Dashboard Running on: http://localhost:${serverConfig.PORT}/ui`);
 
 SampleWorker('SampleQueue');
 
 sampleQueueProducer('SampleJob', {
-    name: "Dhruv",
+    name: "Dhruv N",
     company: "Pryzm",
     position: "Junior Dev",
     location: "Remote | Mumbai | Maharashtra"
